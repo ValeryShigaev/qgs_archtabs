accf876846030e22c0175161e13c2f8b5a57c841
shigaevaler@gmail.com <shigaevaler@gmail.com>
2024-07-20 15:59:02 +0400

    xlwt fix

--- utils/xl_loader.py
+++ utils/xl_loader.py
@@ -1,32 +1,19 @@
-import pip
 from typing import Dict, List
 from .logger import log
 from .decorators import error_handler
-
-try:
-    import xlrd
-except Exception as e:
-    pip.main(['install', 'xlrd'])
-    import xlrd
-    log.error(f"No xlrd module! {e}")
-try:
-    import xlwt
-except Exception as e:
-    pip.main(['install', 'xlwt'])
-    import xlwt
-    log.error(f"No xlwt module! {e}")
+from .xlwt import Workbook
 
 
 class XlHandler:
     """
-    This class allows to write data into excel sheets
+    This class allows to write data into xl sheets
 
     Args:
         self.xl: excel workbook
     """
 
     def __init__(self):
-        self.xl = xlwt.Workbook()
+        self.xl = Workbook()
 
     @error_handler("Excel write sheets")
     def write_data(self, data: Dict[str, List[any]], sheet: str, ) -> None:
